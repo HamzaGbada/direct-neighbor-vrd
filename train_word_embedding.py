a6f171db76f2ed22bb221ae50f505a59aaaf03b8
HamzaGbada <gbadahamza18@gmail.com>
2023-08-31 12:52:10 +0100

    [UPDATE] train_word_embedding.py

--- train_word_embedding.py
+++ train_word_embedding.py
@@ -30,6 +30,7 @@ def train(model, dataloader, loss_fn, optimizer, device):
 
     return total_loss / len(dataloader)
 
+
 def evaluate(model, dataloader, device):
     model.eval()
     all_labels = []
@@ -47,36 +48,41 @@ def evaluate(model, dataloader, device):
             all_labels.extend(labels)
             all_predictions.extend(predictions)
 
-    report = classification_report(all_labels, all_predictions, target_names=["CLASS_1", "CLASS_2", ...])  # Replace with your class names
+    report = classification_report(all_labels, all_predictions,
+                                   target_names=["CLASS_1", "CLASS_2", ...])  # Replace with your class names
     return report
 
 
-def main(dataset):
+def word_embedding_dataloader(dataset, max_len=128, batch_size=16):
     sentences = [x for doc_index in range(len(dataset)) for x in dataset.data[doc_index][1]['text_units']]
     labels = [x for doc_index in range(len(dataset)) for x in dataset.data[doc_index][1]['labels']]
 
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    max_len = 128
-    batch_size = 16
-    num_classes = 5  # Replace with the actual number of classes
 
-    train_dataloader = create_dataloader(sentences, labels, tokenizer, max_len, batch_size)
+    dataloader = create_dataloader(sentences, labels, tokenizer, max_len, batch_size)
+    return dataloader
 
+
+def main(train_dataloader, num_classes=5, num_epochs = 10, device = torch.device('cpu')):
     model = BertForSentenceClassification(num_classes)
     optimizer = AdamW(model.parameters(), lr=2e-5)
     loss_fn = nn.CrossEntropyLoss()
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model.to(device)
 
-    num_epochs = 10
     for epoch in range(num_epochs):
         train_loss = train(model, train_dataloader, loss_fn, optimizer, device)
         logger.debug(f'Epoch [{epoch + 1}/{num_epochs}], Train Loss: {train_loss:.4f}')
 
-    logger.debug(evaluate(model, train_dataloader, device) )
+    logger.debug(f"Train evalution report{evaluate(model, train_dataloader, device)}")
+    return model
 
 
 if __name__ == '__main__':
-    dataset = CORD(train=True)
-    main(dataset)
+    dataset_train = CORD(train=True)
+    dataset_test = CORD(train=False)
+    train_dataloader = word_embedding_dataloader(dataset_train)
+    test_dataloader = word_embedding_dataloader(dataset_test)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = main(train_dataloader)
+    logger.debug(f"Test evalution report{evaluate(model, test_dataloader, device)}")
