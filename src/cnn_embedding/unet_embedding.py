3502f8a2c087ef686282be67df28d39c3c408a4e
HamzaGbada <gbadahamza18@gmail.com>
2023-09-01 12:16:53 +0100

    [UPDATE] unet model

--- src/cnn_embedding/unet_embedding.py
+++ src/cnn_embedding/unet_embedding.py
@@ -1,3 +1,31 @@
 # TODO develop UNET for classification
 #   Remove the last layer for embedding (Same for VGG)
 #   https://saturncloud.io/blog/how-to-remove-the-last-fc-layer-from-a-resnet-model-in-pytorch/
+from torch import nn
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        # Encoder (contracting path)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        # Decoder (expansive path)
+        self.decoder = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, out_channels, kernel_size=3, padding=1),
+            nn.Sigmoid()  # Use Sigmoid for binary classification, adjust for multi-class
+        )
+
+    def forward(self, x):
+        x1 = self.encoder(x)
+        x2 = self.decoder(x1)
+        return x2
