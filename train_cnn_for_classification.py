9890f748de3c04c22cd1830fdf2e73ba5d81e109
HamzaGbada <hamza.gbada@gmail.com>
2023-09-14 10:55:49 +0100

    [UPDATE] image dataloader

--- train_cnn_for_classification.py
+++ train_cnn_for_classification.py
@@ -1,5 +1,8 @@
+import os
+
 import torch
 from torch.utils.data import DataLoader
+from PIL import Image
 
 from src.dataloader.image_classification_dataloader import ImageDataset
 
@@ -49,12 +52,13 @@ def evaluate(model, dataloader, device):
 
 def image_dataloader(dataset, batch_size=1):
     # TODO: crop images from bbox and put them in list of tensor then pass to ImageDataset only the cropped image with label
-    image = None
+    cropped_images = [Image.open(os.path.join(dataset.root, dataset.data[doc_index][0])).crop(bbox) for doc_index in range(len(dataset)) for bbox in dataset.data[doc_index][1]['boxes']]
     boxes = [x for doc_index in range(len(dataset)) for x in dataset.data[doc_index][1]['boxes']]
     labels = [x for doc_index in range(len(dataset)) for x in dataset.data[doc_index][1]['labels']]
-    image_dataset = ImageDataset(image, boxes, labels)
-    dataloader = DataLoader(image_dataset, batch_size=batch_size, shuffle=True)
-    return dataloader
+    text_units = [x for doc_index in range(len(dataset)) for x in dataset.data[doc_index][1]['text_units']]
+    # image_dataset = ImageDataset(image, boxes, labels)
+    # dataloader = DataLoader(image_dataset, batch_size=batch_size, shuffle=True)
+    return cropped_images, boxes, labels, text_units
 
 
 def main(train_dataloader, num_classes=5, num_epochs = 10, device = torch.device('cpu')):
