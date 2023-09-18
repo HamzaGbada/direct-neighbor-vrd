2fcfaba332c60a4aeb5bf334fb89015dd1b1bd01
HamzaGbada <hamza.gbada@gmail.com>
2023-09-18 10:01:46 +0100

    [FIX] WILDRECEIPT dataloader remove little bbox

--- src/dataloader/wildreceipt_dataloader.py
+++ src/dataloader/wildreceipt_dataloader.py
@@ -7,7 +7,7 @@ from doctr.datasets.datasets import VisionDataset
 from doctr.datasets.utils import convert_target_to_relative
 
 from src.utils.setup_logger import logger
-from src.utils.utils import convert_xmin_ymin
+from src.utils.utils import convert_xmin_ymin, get_area
 
 
 class WILDRECEIPT(VisionDataset):
@@ -33,19 +33,20 @@ class WILDRECEIPT(VisionDataset):
             data = file.read()
         # Split the text file into separate JSON strings
         json_strings = data.strip().split('\n')
-        # TODO: Check FUNSD implementation for more details
         for json_string in json_strings:
             json_data = json.loads(json_string)
             file_name = json_data['file_name']
             annotations = json_data['annotations']
-            _targets = [(convert_xmin_ymin(annotation['box']), annotation['text'].lower(), annotation['label']) for annotation in annotations]
-            box_targets, text_units, labels = zip(*_targets)
+            _targets = [(convert_xmin_ymin(annotation['box']), annotation['text'].lower(), annotation['label'])
+                        for annotation in annotations if get_area(convert_xmin_ymin(annotation['box'])) >= 50]
+            if _targets:
+                box_targets, text_units, labels = zip(*_targets)
 
-            self.data.append((
-                file_name,
-                dict(boxes=np.asarray(box_targets, dtype=int), labels=list(labels),
-                     text_units=list(text_units)),
-            ))
+                self.data.append((
+                    file_name,
+                    dict(boxes=np.asarray(box_targets, dtype=int), labels=list(labels),
+                         text_units=list(text_units)),
+                ))
         self.root = tmp_root
 
 
