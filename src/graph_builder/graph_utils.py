a29a01448f24b02191d967bbafa45bc68d88831d
HamzaGbada <hamza.gbada@gmail.com>
2023-11-20 16:29:36 +0100

    [FIX] Never use chatgpt plots

--- src/graph_builder/graph_utils.py
+++ src/graph_builder/graph_utils.py
@@ -17,23 +17,6 @@ def is_connected(box1, box2, all_boxes):
     Returns:
     - True if connected, False otherwise.
     """
-    bounding_boxes_json = {
-        # (6, 1, 10, 10),
-        (11, 15, 15, 10): "mid",  # mid
-        (21, 1, 17, 10): "upper",  # Upper
-        # (10, 70, 25, 10): "super low",  # super low
-        # (30, 12, 25, 10),
-        # (25, 16, 17, 10),
-        # (35, 5, 17, 10),
-        # (20, 32, 5, 5),
-        # (30, 24, 17, 10),
-        # (40, 14, 5, 10),
-        # (60, 34, 17, 10),
-        # (77, 54, 17, 10),
-        # (87, 66, 17, 10),
-        # (90, 74, 17, 10),
-        (21, 32, 17, 10): "low",  # low
-    }
     poly = Polygon(
         [
             (box1[0], box1[1]),
@@ -43,24 +26,11 @@ def is_connected(box1, box2, all_boxes):
             (box1[0], box1[1]),
         ]
     )
-    # plt.plot(*poly.exterior.xy)
-    # logger.debug(f" bounding box one and {bounding_boxes_json[box1]} its coord {box1}")
-    # logger.debug(f" bounding box two and {bounding_boxes_json[box2]} its coord {box2}")
-    b = []
+
     for other_box in all_boxes:
-        # logger.debug(
-        #     f"current box is and ***** {bounding_boxes_json[other_box]} ***** its coord ############ {other_box}"
-        # )
 
         if other_box != box1 and other_box != box2 and poly.is_valid:
-            rectangle = Polygon(
-                [
-                    (other_box[0], other_box[1]),
-                    (other_box[0] + other_box[2], other_box[1]),
-                    (other_box[0] + other_box[2], other_box[1] + other_box[3]),
-                    (other_box[0], other_box[1] + other_box[3]),
-                ]
-            )
+
             point1 = Point(other_box[0], other_box[1])
             point2 = Point(other_box[0] + other_box[2], other_box[1])
             point3 = Point(other_box[0] + other_box[2], other_box[1] + other_box[3])
@@ -70,19 +40,6 @@ def is_connected(box1, box2, all_boxes):
             intersection3 = poly.contains(point3)
             intersection4 = poly.contains(point4)
 
-            intersection = rectangle.intersection(poly)
-            # logger.debug(
-            #     f"intersction between {bounding_boxes_json[box1]} and {bounding_boxes_json[box2]} throw {bounding_boxes_json[other_box]} is {intersection}"
-            # )
-            # if not intersection:
-            #     logger.debug(
-            #         f"No part of the rectangle {bounding_boxes_json[other_box]} is inside the polygon between {bounding_boxes_json[box1]} and {bounding_boxes_json[box2]}"
-            #     )
-            # else:
-            #     logger.debug(
-            #         f"A part of the rectangle {bounding_boxes_json[other_box]} is inside the polygon between {bounding_boxes_json[box1]} and {bounding_boxes_json[box2]}"
-            #     )
-            #     return False, poly
             # Check if the other box is not box1 or box2
             if (
                 not (intersection1)
@@ -91,15 +48,15 @@ def is_connected(box1, box2, all_boxes):
                 and not (intersection4)
             ):
                 logger.debug(
-                    f"No part of the rectangle {bounding_boxes_json[other_box]} is inside the polygon between {bounding_boxes_json[box1]} and {bounding_boxes_json[box2]}"
+                    f"No part of the rectangle is inside the polygon"
                 )
             else:
                 logger.debug(
-                    f"A part of the rectangle {bounding_boxes_json[other_box]} is inside the polygon between {bounding_boxes_json[box1]} and {bounding_boxes_json[box2]}"
+                    f"A part of the rectangle is inside the polygon"
                 )
-                return False, poly
+                return False
 
-    return True, poly
+    return True
 
 
 def connected_boxes(bounding_boxes):
@@ -113,47 +70,13 @@ def connected_boxes(bounding_boxes):
     - List of indices of connected bounding boxes for each bounding box.
     """
     result = []
-    pol = []
-    bounding_boxes_json = {
-        # (6, 1, 10, 10),
-        (11, 15, 15, 10): "mid",  # mid
-        (21, 1, 17, 10): "upper",  # Upper
-        # (10, 70, 25, 10): "super low",  # super low
-        # (30, 12, 25, 10),
-        # (25, 16, 17, 10),
-        # (35, 5, 17, 10),
-        # (20, 32, 5, 5),
-        # (30, 24, 17, 10),
-        # (40, 14, 5, 10),
-        # (60, 34, 17, 10),
-        # (77, 54, 17, 10),
-        # (87, 66, 17, 10),
-        # (90, 74, 17, 10),
-        (21, 32, 17, 10): "low",  # low
-    }
     for i, box1 in enumerate(bounding_boxes):
-        L = []
-        logger.debug(
-            f"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB index {i}: {bounding_boxes_json[box1]}"
-        )
-        for j, box2 in enumerate(bounding_boxes):
-            logger.debug(f" box {bounding_boxes_json[box1]} with index {i} is connected to {bounding_boxes_json[box2]} with index  {j} : {is_connected(box1, box2, bounding_boxes)[0]}")
-            if i!=j and is_connected(box1, box2, bounding_boxes)[0]:
-                # logger.debug(f" after if {is_connected(box1, box2, bounding_boxes)[0]}")
-                L.append(j)
 
-            logger.debug(f"the lis tcontains {L}")
-        # connected_indices = [
-        #     j
-        #     for j, box2 in enumerate(bounding_boxes)
-        #     if i != j and is_connected(box1, box2, bounding_boxes)[0]
-        # ]
-        # logger.debug("USSSSSSSSSSSSSSSSSSEEEEEEEEEEEEELESSSSSS")
-        # pols = [
-        #     is_connected(box1, box2, bounding_boxes)[1]
-        #     for j, box2 in enumerate(bounding_boxes)
-        # ]
-        result.append(L)
-        # result.append(connected_indices)
-        # pol.append(pols)
-    return result, pol
+        connected_indices = [
+            j
+            for j, box2 in enumerate(bounding_boxes)
+            if i != j and is_connected(box1, box2, bounding_boxes)[0]
+        ]
+
+        result.append(connected_indices)
+    return result
