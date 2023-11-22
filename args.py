9384d9c2765a00044954e10835cf90e5611ea703
HamzaGbada <hamza.gbada@gmail.com>
2023-11-22 18:50:40 +0100

    [ADD] args

--- args.py
+++ args.py
@@ -11,15 +11,17 @@ def build_parser():
                         help='Max nodes per node (edges per node)', default=6)
     return parser
 
+
 def build_subparser(subparsers):
     parser_building = subparsers.add_parser('build')
     parser_building.add_argument('-t', '--train', action='store_true',
                                   help='Boolean to choose between the train or test dataset')
     return parser_building
 
+
 def train_subparser(subparsers):
     parser_train = subparsers.add_parser('train')
-    parser_train.add_argument('-d', "--dataname", type=str, default='WILDRECEIPT',
+    parser_train.add_argument('-d', "--dataset", type=str, default='WILDRECEIPT',
                               choices=['FUNSD', 'SROIE', 'CORD', 'WILDRECEIPT'],
                               help="Selecting the dataset for your model's training.")
     parser_train.add_argument('-p', "--path", type=str, default='data/',
@@ -32,31 +34,37 @@ def train_subparser(subparsers):
                               help="The learning rate.")
     parser_train.add_argument('-e', "--epochs", type=int, default=200,
                               help="The number of epochs.")
-    return parser_train
+    return subparsers  # Return the subparsers object for further use
 
-# Main parser
-main_parser = build_parser()
 
-# Create subparsers
-subparser = main_parser.add_subparsers(dest="subcommand", help="Choose subcommand")
-build_subparser(subparser)
-train_subparser(subparser)
+def main():
+    parser = build_parser()
+    subparsers = parser.add_subparsers(dest='subcommand', help='Choose subcommand')
 
-# Parse arguments
-args = main_parser.parse_args()
+    # Create subparsers
+    build_subparser(subparsers)
+    train_subparser(subparsers)
 
-if args.subcommand == "build":
-    # Access build arguments
-    print(args.dataset)
-    print(args.max_node)
-elif args.subcommand == "train":
-    # Access train arguments
-    print(args.dataname)
-    print(args.path)
-    print(args.hidden_size)
-    print(args.hidden_layers)
-    print(args.learning_rate)
-    print(args.epochs)
-else:
-    # Handle invalid subcommands
-    main_parser.print_help()
+    # Parse arguments
+    args = parser.parse_args()
+
+    if args.subcommand == 'build':
+        print(f"Building graph for dataset: {args.dataset}")
+        print(f"Max nodes per node: {args.max_node}")
+        print(f"Train: {args.train}")
+        # Your graph building logic here
+    elif args.subcommand == 'train':
+        print(f"Training model for dataset: {args.dataset}")
+        print(f"Dataset path: {args.path}")
+        print(f"GCN hidden size: {args.hidden_size}")
+        print(f"Number of GCN hidden layers: {args.hidden_layers}")
+        print(f"Learning rate: {args.learning_rate}")
+        print(f"Number of epochs: {args.epochs}")
+        # Your training logic here
+    else:
+        # Handle invalid subcommands
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
--- train.py
+++ train.py
@@ -7,3 +7,15 @@
 #           * Assign Feature vector to each node (use Unet CNN model for embedding)
 #           * Assign edge angle
 #       - Create multiple model for GNN
+
+
+from args import train_subparser, build_parser
+from src.utils.setup_logger import logger
+
+if "__main__" == __name__:
+    main_parser = build_parser()
+    parser = train_subparser(main_parser)
+    args = parser.parse_args()
+
+    logger.debug(args.dataset)
+    logger.debug(args.hidden_size)
