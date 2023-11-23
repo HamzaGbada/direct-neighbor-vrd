77afbcbba507ca64eeed8891b69e118a962fbe0c
HamzaGbada <hamza.gbada@gmail.com>
2023-11-23 17:20:17 +0100

    [ADD] Embedding Model

--- builder.py
+++ builder.py
@@ -2,6 +2,7 @@ import argparse
 
 from args import build_subparser
 from src.utils.setup_logger import logger
+from src.word_embedding.BERT_embedding import TextEmbeddingModel
 
 if __name__ == "__main__":
     main_parser = argparse.ArgumentParser()
@@ -9,6 +10,10 @@ if __name__ == "__main__":
     build_subparser(subparsers)
     args = main_parser.parse_args()
 
+    text_model = TextEmbeddingModel(model_path="CORD_word_classification.pth")
+
+    # Embedding a sentence
+    sentence_embedding = text_model.embed_text("18.167$")
     logger.debug(args.dataset)
 
     # TODO: create the building process
