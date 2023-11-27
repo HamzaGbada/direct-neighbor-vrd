0bf943237ed220e1ba650780b75b89721ae63545
HamzaGbada <hamza.gbada@gmail.com>
2023-11-27 15:44:23 +0100

    [FIX] GCN train

--- train.py
+++ train.py
@@ -4,10 +4,9 @@ import argparse
 import torch
 from dgl import add_self_loop
 from torch.nn import CrossEntropyLoss
-from torch.nn.functional import cross_entropy, relu
+from torch.nn.functional import relu
 from torch.optim import Adam
 from torchmetrics.functional.classification import (
-    multiclass_f1_score,
     multilabel_accuracy,
 )
 
@@ -63,48 +62,48 @@ def train(
 
         # Compute loss
         # Note that you should only compute the losses of the nodes in the training set.
-        logger.debug(f"feature shape labels[train_mask]{labels[train_mask].shape}")
-        logger.debug(f"feature shape logits[train_mask]{logits[train_mask].shape}")
+        logger.debug(f"feature shape labels[train_mask]{labels[train_mask]}")
+        logger.debug(
+            f"feature shape logits[train_mask]{logits[train_mask].squeeze(dim=1)}"
+        )
         # TODO: the error of shape (check the output of the below) is due to the multiclass classification (change
         #  the label)
         # FIXME: RuntimeError: Boolean value of Tensor with more than one value is ambiguous
-        loss = CrossEntropyLoss(logits[train_mask], labels[train_mask])
+        loss = CrossEntropyLoss(logits, labels)
         loss_train.append(loss.to("cpu").detach().numpy())
-        loss_val.append(
-            cross_entropy(logits[val_mask], labels[val_mask]).to("cpu").detach().numpy()
-        )
-        loss_test.append(
-            cross_entropy(logits[test_mask], labels[test_mask])
-            .to("cpu")
-            .detach()
-            .numpy()
-        )
-
-        # Compute accuracy on training/validation/test
-        train_f1 = multiclass_f1_score(
-            pred[train_mask], labels[train_mask], num_classes=num_class, average="micro"
-        )
-        val_f1 = multiclass_f1_score(
-            pred[val_mask], labels[val_mask], num_classes=num_class, average="micro"
-        )
-        test_f1 = multiclass_f1_score(
-            pred[test_mask], labels[test_mask], num_classes=num_class, average="micro"
-        )
-        train_acc = (pred[train_mask] == labels[train_mask]).float().mean()
-        val_acc = (pred[val_mask] == labels[val_mask]).float().mean()
-        # test_acc = (pred[test_mask] == labels[test_mask]).float().mean()
-        train_list.append(train_f1.to("cpu"))
-        val_list.append(val_f1.to("cpu"))
-        test_list.append(test_f1.to("cpu"))
-        # Save the best validation accuracy and the corresponding test accuracy.
-        if best_val_acc < val_acc:
-            best_val_acc = val_acc
-            # best_test_acc = test_acc
-        if best_val_f1 < val_f1:
-            best_val_f1 = val_f1
-
-        if best_test_f1 < test_f1:
-            best_test_f1 = test_f1
+        loss_val.append(loss.to("cpu").detach().numpy())
+        # loss_test.append(
+        #     CrossEntropyLoss(logits[test_mask], class_indices[test_mask])
+        #     .to("cpu")
+        #     .detach()
+        #     .numpy()
+        # )
+        #
+        # # Compute accuracy on training/validation/test
+        # train_f1 = multiclass_f1_score(
+        #     pred[train_mask], class_indices[train_mask], num_classes=num_class, average="micro"
+        # )
+        # val_f1 = multiclass_f1_score(
+        #     pred[val_mask], class_indices[val_mask], num_classes=num_class, average="micro"
+        # )
+        # test_f1 = multiclass_f1_score(
+        #     pred[test_mask], class_indices[test_mask], num_classes=num_class, average="micro"
+        # )
+        # train_acc = (pred[train_mask] == class_indices[train_mask]).float().mean()
+        # val_acc = (pred[val_mask] == class_indices[val_mask]).float().mean()
+        # # test_acc = (pred[test_mask] == class_indices[test_mask]).float().mean()
+        # train_list.append(train_f1.to("cpu"))
+        # val_list.append(val_f1.to("cpu"))
+        # test_list.append(test_f1.to("cpu"))
+        # # Save the best validation accuracy and the corresponding test accuracy.
+        # if best_val_acc < val_acc:
+        #     best_val_acc = val_acc
+        #     # best_test_acc = test_acc
+        # if best_val_f1 < val_f1:
+        #     best_val_f1 = val_f1
+        #
+        # if best_test_f1 < test_f1:
+        #     best_test_f1 = test_f1
 
         # Backward
         optimizer.zero_grad()
@@ -113,10 +112,10 @@ def train(
         logger.debug(f"Best Test f1-score {best_test_f1}")
         # FIXME: make e%10
         if e % 1 == 0:
-            logger.debug(
-                f"Epochs: {e}/{epochs}, Train F1-score: {train_f1}, Val F1-score: {val_f1}, Train Accuracy: "
-                f"{train_acc}, Val Accuracy: {val_acc}, Best Accuracy: {best_val_acc}, Best F1-score: {best_val_f1}, Best Test F1-score: {best_test_f1}"
-            )
+            # logger.debug(
+            #     f"Epochs: {e}/{epochs}, Train F1-score: {train_f1}, Val F1-score: {val_f1}, Train Accuracy: "
+            #     f"{train_acc}, Val Accuracy: {val_acc}, Best Accuracy: {best_val_acc}, Best F1-score: {best_val_f1}, Best Test F1-score: {best_test_f1}"
+            # )
             logger.debug(
                 f"Epochs: {e}/{epochs}, ############# Train F1-score: {f1_score_train}"
                 f"{accuracy_train}"
@@ -146,8 +145,8 @@ if __name__ == "__main__":
     val_mask = dataset.val_mask
     test_mask = dataset.test_mask
     # test_index = torch.arange(graph_test.number_of_nodes())
-    logger.debug(f"data training indexes {train_mask}")
-    logger.debug(f"data validiation indexes {val_mask}")
+    # logger.debug(f"data training indexes {train_mask}")
+    # logger.debug(f"data validiation indexes {val_mask}")
     # logger.debug(f"data testing indexes {test_index}")
 
     model = WGCN(
