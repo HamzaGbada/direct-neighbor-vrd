5cc5bfaf615c724402a1c62b8eb9a849c48d20b8
HamzaGbada <hamza.gbada@gmail.com>
2024-01-13 12:03:29 +0100

    [TEST] xfund

--- src/dataloader/xfund_dataloader.py
+++ src/dataloader/xfund_dataloader.py
@@ -1,81 +1,100 @@
 import json
 import os
-from typing import Any, List, Tuple, Dict
+from pathlib import Path
+from typing import Any, List, Tuple, Dict, Union
 
 import numpy as np
 from doctr.datasets.datasets import VisionDataset
 from doctr.datasets.utils import convert_target_to_relative
 
+from src.utils.setup_logger import logger
 from src.utils.utils import convert_xmin_ymin, get_area
 
 
 class XFUND(VisionDataset):
     """
+    >>> # NOTE: You need to download the dataset first.
+    >>> train_set = XFUND(train=True, data_folder="data/fr.val.json")
+    >>> img, target = train_set[0]
+    >>> test_set = XFUND(train=False, data_folder="data/fr.val.json")
     :return:
         Bounding boxes are in the Format (xmin, ymin, xmax, ymax) top left, bottom right corners
     """
 
-    TRAIN = (
-        "https://github.com/doc-analysis/XFUND/releases/download/v1.0/fr.train.json",
-        "fr.train.json",
-    )
-
-    TEST = (
-        "https://github.com/doc-analysis/XFUND/releases/download/v1.0/fr.val.json",
-        "fr.val.json",
-    )
-
-    def __init__(self, train: bool = True, **kwargs: Any) -> None:
-        url, filename = self.TRAIN if train else self.TEST
+    def __init__(
+        self,
+        data_folder: str,
+        train: bool = True,
+        use_polygons: bool = False,
+        recognition_task: bool = False,
+        **kwargs: Any,
+    ) -> None:
         super().__init__(
-            url,
-            filename,
-            None,
-            True,
-            pre_transforms=convert_target_to_relative,
+            data_folder,
+            pre_transforms=convert_target_to_relative if not recognition_task else None,
             **kwargs,
         )
+        # File existence check
+        if not os.path.exists(data_folder):
+            raise FileNotFoundError(
+                f"unable to locate {data_folder}"
+            )
 
-        # tmp_root = os.path.join(self.root, "wildreceipt/")
+        tmp_root = data_folder
         self.train = train
+        np_dtype = np.float32
+        self.data: List[
+            Tuple[Union[str, Path, np.ndarray], Union[str, Dict[str, Any]]]
+        ] = []
 
-        self.data: List[Tuple[str, Dict[str, Any]]] = []
-
-        # self.filename = "train.txt" if self.train else "test.txt"
-        # file_path = os.path.join(tmp_root, self.filename)
-        # # logger.debug(f'the file names: {tmp_root}')
-        # with open(file_path, "r") as file:
-        #     data = file.read()
-        # # Split the text file into separate JSON strings
-        # json_strings = data.strip().split("\n")
+        with open(data_folder, "r") as file:
+            data = file.read()
+        # Split the text file into separate JSON strings
+        json_strings = data.strip().split("\n")
+        box: Union[List[float], np.ndarray]
+        _targets = []
+        logger.debug(f"data {data}")
         # for json_string in json_strings:
         #     json_data = json.loads(json_string)
-        #     file_name = json_data["file_name"]
+        #     img_path = json_data["file_name"]
         #     annotations = json_data["annotations"]
-        #     _targets = [
-        #         (
-        #             convert_xmin_ymin(annotation["box"]),
-        #             annotation["text"].lower(),
-        #             annotation["label"],
-        #         )
-        #         for annotation in annotations
-        #         if get_area(convert_xmin_ymin(annotation["box"])) >= 50
-        #     ]
-        #     if _targets:
-        #         box_targets, text_units, labels = zip(*_targets)
-        #         if (
-        #             len(box_targets) > 1
-        #         ):  # number of bounding boxes in document should be more than one
-        #             self.data.append(
-        #                 (
-        #                     file_name,
-        #                     dict(
-        #                         boxes=np.asarray(box_targets, dtype=int),
-        #                         labels=list(labels),
-        #                         text_units=list(text_units),
-        #                     ),
-        #                 )
+        #     for annotation in annotations:
+        #         coordinates = annotation["box"]
+        #         if use_polygons:
+        #             # (x, y) coordinates of top left, top right, bottom right, bottom left corners
+        #             box = np.array(
+        #                 [
+        #                     [coordinates[0], coordinates[1]],
+        #                     [coordinates[2], coordinates[3]],
+        #                     [coordinates[4], coordinates[5]],
+        #                     [coordinates[6], coordinates[7]],
+        #                 ],
+        #                 dtype=np_dtype,
         #             )
+        #         else:
+        #             x, y = coordinates[::2], coordinates[1::2]
+        #             box = [min(x), min(y), max(x), max(y)]
+        #         _targets.append((annotation["text"], box))
+        #     text_targets, box_targets = zip(*_targets)
+        #
+        #     if recognition_task:
+        #         crops = crop_bboxes_from_image(
+        #             img_path=os.path.join(tmp_root, img_path),
+        #             geoms=np.asarray(box_targets, dtype=int).clip(min=0),
+        #         )
+        #         for crop, label in zip(crops, list(text_targets)):
+        #             if label and " " not in label:
+        #                 self.data.append((crop, label))
+        #     else:
+        #         self.data.append(
+        #             (
+        #                 img_path,
+        #                 dict(
+        #                     boxes=np.asarray(box_targets, dtype=int).clip(min=0),
+        #                     labels=list(text_targets),
+        #                 ),
+        #             )
+        #         )
         # self.root = tmp_root
 
     def extra_repr(self) -> str:
