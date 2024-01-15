115dd1e75af060682b07f885e310f0839711d739
HamzaGbada <hamza.gbada@gmail.com>
2024-01-15 10:05:16 +0100

    [FIX] training

--- build_graphs.sh
+++ build_graphs.sh
@@ -2,11 +2,11 @@
 source venv/bin/activate
 python train_word_embedding.py embed -d FUNSD -e 25
 
-mv file.log data/file_XFUND_embedding.log
+mv file.log data/file_FUNSD_embedding.log
 
 python train_word_embedding.py embed -d XFUND -e 25
 
-mv file.log data/file_FUNSD_embedding.log
+mv file.log data/file_XFUND_embedding.log
 
 python builder.py build -d XFUND
 python builder.py build -d FUNSD
